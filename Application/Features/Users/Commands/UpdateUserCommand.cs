d06597608688acf6b46decba54b75eb8d4a0feb6
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-01 15:53:39 -0600

    Add Update User Command Behavior

---
+++ Application/Features/Users/Commands/UpdateUserCommand.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Dto;
+using LanguageExt.Common;
+using MediatR;
+
+namespace Application.Features.Users.Commands
+{
+    public class UpdateUserCommand : IRequest<Result<UserDto>>
+    {
+        public Guid UserId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Lastname { get; set; } = null!;
+
+        public string Username { get; set; } = null!;
+
+    }
+}
---
+++ Application/Features/Users/Validators/UpdateUserComamandValidator.cs
@@ -0,0 +1,32 @@
+﻿using Application.Features.Users.Commands;
+using Domain.Interfaces;
+using FluentValidation;
+
+namespace Application.Features.Users.Validators
+{
+    public class UpdateUserComamandValidator : AbstractValidator<UpdateUserCommand>
+    {
+        public UpdateUserComamandValidator(IUnitOfWork unitOfWork)
+        {
+            RuleLevelCascadeMode = CascadeMode.Stop;
+
+            RuleFor(u => u.UserId)
+                .NotEmpty()
+                .MustAsync(async (id, cancellationToken) => await unitOfWork.UserRepository.AnyAsync(u => u.UserId == id, cancellationToken))
+                .WithMessage("Usuario no encontrado");
+
+            RuleFor(r => r.Username)
+                .MaximumLength(100).WithMessage("La longitud máxima de {PropertyName} es de 100")
+                .MustAsync(async (username, ctx) =>
+                    !await unitOfWork.UserRepository.AnyAsync(u => u.Username == username, ctx))
+                .WithMessage("{PropertyValue} ya existe");
+
+            RuleFor(r => r.Name)
+                .MaximumLength(100).WithMessage("La longitud máxima de {PropertyName} es de 100");
+
+            RuleFor(r => r.Lastname)
+                .MaximumLength(100).WithMessage("La longitud máxima de {PropertyName} es de 100");
+
+        }
+    }
+}
