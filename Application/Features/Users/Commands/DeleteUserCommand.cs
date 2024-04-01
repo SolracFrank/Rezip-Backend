5c9c2b8d1be05ef8a2216baa93ac12e062d6bd5a
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-01 15:53:27 -0600

    Add Delete User Command Behavior

---
+++ Application/Features/Users/Commands/DeleteUserCommand.cs
@@ -0,0 +1,10 @@
+﻿using LanguageExt.Common;
+using MediatR;
+
+namespace Application.Features.Users.Commands
+{
+    public class DeleteUserCommand : IRequest<Result<string>>
+    {
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Application/Features/Users/Validators/DeleteUserCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Users.Commands;
+using Domain.Interfaces;
+using FluentValidation;
+
+namespace Application.Features.Users.Validators
+{
+    internal class DeleteUserCommandValidator : AbstractValidator<DeleteUserCommand>
+    {
+        public DeleteUserCommandValidator(IUnitOfWork unitOfWork)
+        {
+            CascadeMode = CascadeMode.Stop;
+
+            RuleFor(r => r.UserId)
+                .NotEmpty()
+                .MustAsync(async (id, ctx) => await unitOfWork.UserRepository.AnyAsync(u => u.UserId == id, ctx))
+                .WithMessage("El usuario a eliminar no existe");
+        }
+    }
+}
