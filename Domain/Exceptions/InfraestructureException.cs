25abc8a59a4e13968bf36c0938bb5236de7f14e9
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-01 14:38:54 -0600

    Add exception models

---
+++ Domain/Exceptions/InfraestructureException.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Exceptions;
+
+public class InfraestructureException : Exception
+{
+    public InfraestructureException()
+    {
+    }
+
+    public InfraestructureException(string message) : base(message)
+    {
+    }
+}
---
+++ Domain/Exceptions/ValidationException.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Exceptions;
+
+public class ValidationException : Exception
+{
+    public ValidationException()
+    {
+    }
+
+    public ValidationException(string message) : base(message)
+    {
+    }
+
+    public ValidationException(string keyName, IEnumerable<string> errorMessages)
+    {
+        Errors ??= new Dictionary<string, IEnumerable<string>>();
+
+        Errors.Add(keyName, errorMessages);
+    }
+
+    public IDictionary<string, IEnumerable<string>> Errors { get; set; }
+}
