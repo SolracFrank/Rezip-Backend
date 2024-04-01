3a9b9ccd39cad57a58c3c73e595e058fa6dbb87b
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-01 15:54:12 -0600

    Add Get Users Query Behavior

---
+++ Application/Features/Users/Handlers/GetUsersQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Data;
+
+namespace Application.Features.Users.Handlers;
+using Application.Features.Users.Queries;
+using Domain.Dto;
+using Domain.Dto.Extensions;
+using Domain.Interfaces;
+using LanguageExt.Common;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, Result<IEnumerable<UserDto>>>
+{
+    private readonly ILogger<GetUsersQueryHandler> _logger;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly RecipesDbContext _context;
+
+    public GetUsersQueryHandler(ILogger<GetUsersQueryHandler> logger, IUnitOfWork unitOfWork, RecipesDbContext context)
+    {
+        _logger = logger;
+        _unitOfWork = unitOfWork;
+        _context = context;
+    }
+    public async Task<Result<IEnumerable<UserDto>>> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+    {
+        var users = await _unitOfWork.UserRepository.GetAllAsync(cancellationToken, true);
+
+        //var usersDto = Mapper.Map<User, UserDto>(users);
+        var usersDto = users.ToUserDto();
+
+        return new Result<IEnumerable<UserDto>>(usersDto);
+    }
+}
---
+++ Application/Features/Users/Queries/GetUsersQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Features.Users.Queries;
+
+using Domain.Dto;
+using Domain.Entities;
+using LanguageExt.Common;
+using MediatR;
+
+public class GetUsersQuery : IRequest<Result<IEnumerable<UserDto>>>
+{
+
+}
