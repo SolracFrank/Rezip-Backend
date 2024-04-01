b3c817fe06c78fba7d2cb62735f9ec46ff5750d0
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-01 14:47:29 -0600

    add user extension

---
+++ Domain/Dto/Extensions/UserExtension.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+
+namespace Domain.Dto.Extensions
+{
+    public static class UserExtension
+    {
+        public static UserDto ToUserDto(this User user)
+        {
+            return new UserDto
+            {
+                UserId = user.UserId,
+                Lastname =user.Lastname,
+                Name = user.Name,
+                Username = user.Username,
+                CreatedBy = user.CreatedByNavigation?.Username
+            };
+        }
+        public static IEnumerable<UserDto> ToUserDto(this IEnumerable<User> files)
+        {
+            return files.Select(file => file.ToUserDto());
+        }
+    }
+}
