2c174a24eadb43e5ea8a8f7f9b7bd3d85a2aa85a
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-01 14:42:52 -0600

    Add Controller extension To convert Language.Common Result to ActionResult

---
+++ WebApi/Controllers/ControllerExtension.cs
@@ -0,0 +1,54 @@
+﻿namespace WebApi.Controllers;
+using Domain.Exceptions;
+using LanguageExt.Common;
+using Microsoft.AspNetCore.Mvc;
+using Validation;
+using ValidationException = FluentValidation.ValidationException;
+
+public static class ControllerExtensions
+{
+    public static IActionResult ToOk<TResult, TContract>(this Result<TResult> result,
+        Func<TResult, TContract> mapper)
+    {
+        return result.Match(obj =>
+        {
+            var response = mapper(obj);
+
+            return new OkObjectResult(response);
+        }, GenerateActionResult);
+    }
+
+    public static IActionResult ToOk<TResult>(this Result<TResult> result)
+    {
+        return result.Match(response => new OkObjectResult(response), GenerateActionResult);
+    }
+
+    private static IActionResult GenerateActionResult(this Exception exception)
+    {
+        switch (exception)
+        {
+            case ValidationException validationException:
+                return new BadRequestObjectResult(validationException.ToProblemDetails());
+
+            case Domain.Exceptions.ValidationException validationException:
+                return new BadRequestObjectResult(validationException.ToProblemDetails());
+
+            case InfraestructureException infrastructureException:
+            {
+                var problemDetails = new ProblemDetails();
+                var statusCode = StatusCodes.Status500InternalServerError;
+
+                problemDetails.Detail = exception.Message;
+                problemDetails.Title = infrastructureException.Message;
+
+                return new ObjectResult(problemDetails) { StatusCode = statusCode };
+            }
+
+            case UnAuthorizedException unauthorizedException:
+                return new ObjectResult(unauthorizedException.ToProblemDetails());
+
+            default:
+                return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
