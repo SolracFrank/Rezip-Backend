d2d80e5145f20d144a310b62269430b2efc38ff4
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-01 14:40:03 -0600

    Add Unit of Work

---
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Interfaces;
+
+using Entities;
+
+public interface IUnitOfWork : IDisposable
+{
+    IRepository<Recipe> RecipeRepository { get; }
+
+    IRepository<User> UserRepository { get; }
+
+    bool SaveChanges();
+
+    Task<bool> SaveChangesAsync(CancellationToken cancellationToken);
+}
