47a4d8aeb15ab719bc1e7edf8b51f0ad79fc742e
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-01 14:39:37 -0600

    Add repository

---
+++ Domain/Interfaces/IRepository.cs
@@ -0,0 +1,34 @@
+﻿namespace Domain.Interfaces;
+
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken);
+
+    Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken);
+
+    Task<bool> AllAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+
+    Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+
+    Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken cancellationToken, bool? asNoTracking = null);
+
+    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken, bool? asNoTracking = null);
+
+    void Remove(TEntity entity);
+
+    void RemoveRange(IEnumerable<TEntity> entities);
+
+    void Update(TEntity entity);
+
+    void UpdateRange(IEnumerable<TEntity> entities);
+
+    Task<IEnumerable<TEntity>> WhereAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken cancellationToken, bool? asNoTracking = null);
+
+    Task<TEntity?> FindAsync(CancellationToken cancellationToken, params object[] id);
+
+}
---
+++ Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,136 @@
+﻿namespace Infrastructure.Services;
+
+using Data;
+using Domain.Exceptions;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Linq.Expressions;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private const string DefaultDatabaseErrorMessage = "Communication error with database has ocurred.";
+    protected readonly RecipesDbContext Context;
+    protected readonly DbSet<T> Entities;
+    protected readonly ILogger<Repository<T>> Logger;
+
+    public Repository(RecipesDbContext context, ILogger<Repository<T>> logger)
+    {
+        Context = context;
+        Logger = logger;
+        Entities = context.Set<T>();
+    }
+
+    public async Task AddAsync(T entity, CancellationToken cancellationToken)
+    {
+        await Entities.AddAsync(entity, cancellationToken);
+    }
+
+    public async Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken)
+    {
+        await Entities.AddRangeAsync(entities, cancellationToken);
+    }
+
+    public async Task<bool> AllAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await Entities.AllAsync(predicate, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "An exception has occurred {Message}.", ex.Message);
+
+            throw new InfraestructureException(DefaultDatabaseErrorMessage);
+        }
+    }
+
+    public async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await Entities.AnyAsync(predicate, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "An exception has occurred {Message}.", ex.Message);
+
+            throw new InfraestructureException(DefaultDatabaseErrorMessage);
+        }
+    }
+
+    public async Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate,
+        CancellationToken cancellationToken, bool? asNoTracking = null)
+    {
+        try
+        {
+            return asNoTracking is null or false
+                ? await Entities.FirstOrDefaultAsync(predicate, cancellationToken)
+                : await Entities.AsNoTracking().FirstOrDefaultAsync(predicate, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "An exception has occurred {Message}.", ex.Message);
+
+            throw new InfraestructureException(DefaultDatabaseErrorMessage);
+        }
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken, bool? asNoTracking = null)
+    {
+        try
+        {
+            return asNoTracking is null or false
+                ? await Entities.ToListAsync(cancellationToken)
+                : await Entities.AsNoTracking().ToListAsync(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "An exception has occurred {Message}.", ex.Message);
+
+            throw new InfraestructureException(DefaultDatabaseErrorMessage);
+        }
+    }
+
+    public void Remove(T entity)
+    {
+        Entities.Remove(entity);
+    }
+
+    public void RemoveRange(IEnumerable<T> entities)
+    {
+        Entities.RemoveRange(entities);
+    }
+
+    public void Update(T entity)
+    {
+        Entities.Update(entity);
+    }
+
+    public void UpdateRange(IEnumerable<T> entities)
+    {
+        Entities.UpdateRange(entities);
+    }
+
+    public async Task<IEnumerable<T>> WhereAsync(Expression<Func<T, bool>> predicate,
+        CancellationToken cancellationToken, bool? asNoTracking = null)
+    {
+        try
+        {
+            return asNoTracking is null or false
+                ? await Entities.Where(predicate).ToListAsync(cancellationToken)
+                : await Entities.Where(predicate).AsNoTracking().ToListAsync(cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "An exception has occurred {Message}.", ex.Message);
+
+            throw new InfraestructureException(DefaultDatabaseErrorMessage);
+        }
+    }
+
+    public async Task<T?> FindAsync(CancellationToken cancellationToken, params object[] id)
+    {
+        return await Entities.FindAsync(id, cancellationToken);
+    }
+}
