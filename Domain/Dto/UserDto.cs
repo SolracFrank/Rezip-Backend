7339c0e34de4c8c609ee7e8f3e42da44f3110704
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-01 14:46:58 -0600

    add User db management

---
+++ Domain/Dto/UserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Dto
+{
+    public class UserDto
+    {
+        public Guid UserId { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Lastname { get; set; } = null!;
+
+        public string Username { get; set; } = null!;
+        public string? CreatedBy { get; set; } = null!;
+
+    }
+}
