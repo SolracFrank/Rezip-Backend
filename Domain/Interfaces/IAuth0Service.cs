62c39b31ead96052b5f990bffef908d308824b29
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-05 10:47:55 -0600

    add Auth0Services

---
+++ Domain/Interfaces/IAuth0Service.cs
@@ -0,0 +1,12 @@
+﻿using LanguageExt.Common;
+
+namespace Domain.Interfaces
+{
+    public interface IAuth0Service
+    {
+        public Task<string> GetAuth0Token();
+        public Task<Result<string>> GiveUserPermissions(string userId);
+        public Task<Result<string>> DeleteUser(string userId);
+
+    }
+}
