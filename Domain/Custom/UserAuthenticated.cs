1b1d086693d1aa0c7aabaa225a4ed461a7c4f4c0
Carlos Francisco Herrera Franco <carlosfranciscoherrerafranco@gmail.com>
2024-04-05 10:48:29 -0600

    update user authenticated custom entity

--- Domain/Custom/UserAuthenticated.cs
+++ Domain/Custom/UserAuthenticated.cs
@@ -2,7 +2,7 @@
 {
     public class UserAuthenticated
     {
-        public Guid UserId { get; set; }
+        public string UserId { get; set; }
         public string Name { get; set; }
     }
 }
